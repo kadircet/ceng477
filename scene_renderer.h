58ac55cb434a0409059f2e73cd17686cd62065c8
kadircet <kadir.cetinkaya@prodaft.com>
2017-10-22 17:07:45 +0300

    interface

--- scene_renderer.h
+++ scene_renderer.h
@@ -1,17 +1,32 @@
 #ifndef _SCENE_RENDERER_H
 #define _SCENE_RENDERER_H
 
+struct Pixel {
+  unsigned char r,g,b;
+};
+
 class SceneRenderer {
 private:
-  parser::Scene scene;
+  parser::Scene scene_;
+
+  bool DoesIntersect(const Vec3f& e, const Vec3f& s, const Mesh& mesh);
+  bool DoesIntersect(const Vec3f& e, const Vec3f& s, const Triangle& triangle);
+  bool DoesIntersect(const Vec3f& e, const Vec3f& s, const Sphere& sphere);
+
+  Vec3f CalculateS(int i, int j, const parser::Camera& camera);
+
+  Pixel RenderPixel(int i, int j, const parser::Camera& camera);
+  
 public:
   SceneRenderer(const string& scene_path) {
-    scene.loadFromXml(scene_path);
+    scene_.loadFromXml(scene_path);
   }
 
   const std::vector<parser::Camera>& Cameras() {
     return scene.cameras;
   }
+
+  Pixel* RenderImage(const parser::Camera& camera);
 };
 
 #endif
