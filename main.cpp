a3b206e54e19ec60bd545aab0082992cc1111bad
kadircet <kadir.cetinkaya@prodaft.com>
2017-10-22 16:33:46 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "parser.h"
+#include "ppm.h"
+
+typedef unsigned char RGB[3];
+
+class SceneRenderer {
+private:
+  parser::Scene scene;
+public:
+  SceneRenderer(const string& scene_path) {
+    scene.loadFromXml(scene_path);
+  }
+
+  const std::vector<parser::Camera>& Cameras() {
+    return scene.cameras;
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  SceneRenderer scene_renderer(argv[1]);
+
+  const RGB BAR_COLOR[8] =
+   {
+        { 255, 255, 255 },  // 100% White
+        { 255, 255,   0 },  // Yellow
+        {   0, 255, 255 },  // Cyan
+        {   0, 255,   0 },  // Green
+        { 255,   0, 255 },  // Magenta
+        { 255,   0,   0 },  // Red
+        {   0,   0, 255 },  // Blue
+        {   0,   0,   0 },  // Black
+    };
+
+    int width = 640, height = 480;
+    int columnWidth = width / 8;
+
+    unsigned char* image = new unsigned char [width * height * 3];
+
+    int i = 0;
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int colIdx = x / columnWidth;
+            image[i++] = BAR_COLOR[colIdx][0];
+            image[i++] = BAR_COLOR[colIdx][1];
+            image[i++] = BAR_COLOR[colIdx][2];
+        }
+    }
+
+    write_ppm(argv[2], image, width, height);
+
+}
