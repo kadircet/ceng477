30cbb3ad1aa7a66ed23dc69f27a734350d409cfe
Bilal Yaylak <bilalyaylak@gmail.com>
2017-11-03 21:17:34 +0300

    removed couts

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
-#include <ctime>
-#include <iostream>
 #include "parser.h"
 #include "ppm.h"
 #include "scene_renderer.h"
+#include <iostream>
 using namespace parser;
 
-int main(int argc, char* argv[]) {
-  time_t begin = time(NULL);
+int main(int argc, char *argv[]) {
   SceneRenderer scene_renderer(argv[1]);
 
-  for (const Camera& camera : scene_renderer.Cameras()) {
-    const Vec3i* pixels = scene_renderer.RenderImage(camera);
+  for (const Camera &camera : scene_renderer.Cameras()) {
+    const Vec3i *pixels = scene_renderer.RenderImage(camera);
     const int width = camera.image_width;
     const int height = camera.image_height;
-    unsigned char* image = new unsigned char[width * height * 3];
+    unsigned char *image = new unsigned char[width * height * 3];
 
     int idx = 0;
     for (int i = 0; i < width; i++) {
@@ -28,9 +26,5 @@ int main(int argc, char* argv[]) {
     write_ppm(camera.image_name.c_str(), image, width, height);
     delete[] image;
   }
-  time_t end = time(NULL);
-  int elapsed_seconds = difftime(end, begin);
-  std::cout << elapsed_seconds / 60 << " minutes ";
-  std::cout << elapsed_seconds % 60 << " seconds" << std::endl;
   return 0;
 }
